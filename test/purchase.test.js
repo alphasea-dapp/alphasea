b05789b93b7f2fa5d70f71eb85ad093315884acc
a <a@example.com>
2022-01-15 22:39:57 +0900

    add test for shipPurchases and refundPurchases

--- test/purchase.test.js
+++ test/purchase.test.js
@@ -13,6 +13,7 @@ describe("purchase", function () {
     const addresses = await ethers.getSigners()
     this.myAddress = addresses[0]
     this.otherAddress = addresses[1]
+    this.otherAddress2 = addresses[2]
 
     this.alphasea = await this.Alphasea.deploy(defaultTournaments);
     await this.alphasea.deployed();
@@ -40,6 +41,10 @@ describe("purchase", function () {
     const baseTime = Math.floor(2000000000 / daySeconds) * daySeconds;
     this.executionStartAt = baseTime + 60 * 60
     this.predictionStartAt = this.executionStartAt - 4 * 15 * 60
+    this.purchaseStartAt = this.executionStartAt - 3 * 15 * 60
+    this.shippingStartAt = this.executionStartAt - 2 * 15 * 60
+    this.refundStartAt = this.executionStartAt - 1 * 15 * 60
+
     await ethers.provider.send("evm_setNextBlockTimestamp", [this.predictionStartAt])
 
     // predict
@@ -55,18 +60,17 @@ describe("purchase", function () {
       price: 2,
     }])).wait()
 
-    this.purchaseStartAt = this.executionStartAt - 3 * 15 * 60
     await ethers.provider.send("evm_setNextBlockTimestamp", [this.purchaseStartAt])
   });
 
   describe("createPurchases", function () {
     it("empty", async function () {
-      await expect(this.alphasea.createPurchases([]))
+      await expect(this.alphasea.connect(this.otherAddress).createPurchases([]))
           .to.be.revertedWith('empty params');
     });
 
     it("model not exist", async function () {
-      await expect(this.alphasea.createPurchases([{
+      await expect(this.alphasea.connect(this.otherAddress).createPurchases([{
         modelId: 'not_found',
         executionStartAt: this.executionStartAt,
         publicKey: [1, 2, 3],
@@ -190,59 +194,284 @@ describe("purchase", function () {
               this.otherAddress.address,
               '0x040506',
           );
+
+      expect(await this.alphasea.provider.getBalance(this.alphasea.address), 3)
     });
   })
 
   describe("shipPurchases", function () {
+    beforeEach(async function () {
+      await (await this.alphasea.connect(this.otherAddress).createPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+        publicKey: [1, 2, 3],
+      }, {
+        modelId: 'model2',
+        executionStartAt: this.executionStartAt,
+        publicKey: [4, 5, 6],
+      }], { 'value': 3 })).wait()
+
+      await ethers.provider.send("evm_setNextBlockTimestamp", [this.shippingStartAt])
+    })
+
     it("empty", async function () {
       await expect(this.alphasea.shipPurchases([]))
           .to.be.revertedWith('empty params');
     });
 
     it("model not exist", async function () {
+      await expect(this.alphasea.shipPurchases([{
+        modelId: 'not_found',
+        executionStartAt: this.executionStartAt,
+        purchaser: this.otherAddress.address,
+        encryptedContentKey: [1, 2, 3],
+      }]))
+          .to.be.revertedWith('modelId not exist.');
     });
+
     it("not model owner", async function () {
+      await expect(this.alphasea.connect(this.otherAddress2).shipPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+        purchaser: this.otherAddress.address,
+        encryptedContentKey: [1, 2, 3],
+      }]))
+          .to.be.revertedWith('model owner only.');
     });
+
     it("empty encryptedContentKey", async function () {
+      await expect(this.alphasea.shipPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+        purchaser: this.otherAddress.address,
+        encryptedContentKey: [],
+      }]))
+          .to.be.revertedWith('encryptedContentKey empty.');
     });
+
     it("too early", async function () {
+      await ethers.provider.send("evm_setNextBlockTimestamp",
+          [this.shippingStartAt - 1])
+
+      await expect(this.alphasea.shipPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+        purchaser: this.otherAddress.address,
+        encryptedContentKey: [1, 2, 3],
+      }]))
+          .to.be.revertedWith('shipPurchase is forbidden now');
     });
+
     it("too late", async function () {
+      await ethers.provider.send("evm_setNextBlockTimestamp",
+          [this.shippingStartAt + 15 * 60])
+
+      await expect(this.alphasea.shipPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+        purchaser: this.otherAddress.address,
+        encryptedContentKey: [1, 2, 3],
+      }]))
+          .to.be.revertedWith('shipPurchase is forbidden now');
     });
+
     it("purchase not exist", async function () {
+      await expect(this.alphasea.shipPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+        purchaser: this.otherAddress2.address,
+        encryptedContentKey: [1, 2, 3],
+      }]))
+          .to.be.revertedWith('Purchase not found.');
     });
+
     it("already shipped", async function () {
+      await (await this.alphasea.shipPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+        purchaser: this.otherAddress.address,
+        encryptedContentKey: [1, 2, 3],
+      }])).wait()
+
+      await expect(this.alphasea.shipPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+        purchaser: this.otherAddress.address,
+        encryptedContentKey: [1, 2, 3],
+      }]))
+          .to.be.revertedWith('Already shipped.');
     });
+
     it("already refunded", async function () {
       // impossible
     });
+
     it("reentrance", async function () {
+      // TODO
     });
+
     it("ok", async function () {
+      await expect(this.alphasea.shipPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+        purchaser: this.otherAddress.address,
+        encryptedContentKey: [1, 2, 3],
+      }, {
+        modelId: 'model2',
+        executionStartAt: this.executionStartAt,
+        purchaser: this.otherAddress.address,
+        encryptedContentKey: [4, 5, 6],
+      }]))
+          .to.emit(this.alphasea, 'PurchaseShipped')
+          .withArgs(
+              'model1',
+              this.executionStartAt,
+              this.otherAddress.address,
+              '0x010203',
+          )
+          .to.emit(this.alphasea, 'PurchaseShipped')
+          .withArgs(
+              'model2',
+              this.executionStartAt,
+              this.otherAddress.address,
+              '0x040506',
+          );
+    });
+
+    it("ok. check balance", async function () {
+      await expect(await this.alphasea.shipPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+        purchaser: this.otherAddress.address,
+        encryptedContentKey: [1, 2, 3],
+      }, {
+        modelId: 'model2',
+        executionStartAt: this.executionStartAt,
+        purchaser: this.otherAddress.address,
+        encryptedContentKey: [4, 5, 6],
+      }]))
+          .to.changeEtherBalances(
+              [this.myAddress, this.alphasea],
+              [3, -3]
+          )
     });
   })
 
   describe("refundPurchases", function () {
+    beforeEach(async function () {
+      await (await this.alphasea.connect(this.otherAddress).createPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+        publicKey: [1, 2, 3],
+      }, {
+        modelId: 'model2',
+        executionStartAt: this.executionStartAt,
+        publicKey: [4, 5, 6],
+      }], { 'value': 3 })).wait()
+
+      await ethers.provider.send("evm_setNextBlockTimestamp", [this.refundStartAt])
+    })
+
     it("empty", async function () {
-      await expect(this.alphasea.refundPurchases([]))
+      await expect(this.alphasea.connect(this.otherAddress).refundPurchases([]))
           .to.be.revertedWith('empty params');
     });
 
     it("model not exist", async function () {
+      await expect(this.alphasea.connect(this.otherAddress).refundPurchases([{
+        modelId: 'not_found',
+        executionStartAt: this.executionStartAt,
+      }]))
+          .to.be.revertedWith('modelId not exist.');
     });
-    it("not purchaser", async function () {
+
+    it("not purchaser / purchase not exist", async function () {
+      await expect(this.alphasea.connect(this.otherAddress2).refundPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+      }]))
+          .to.be.revertedWith('Purchase not found.');
     });
+
     it("too early", async function () {
+      await ethers.provider.send("evm_setNextBlockTimestamp",
+          [this.refundStartAt - 1])
+
+      await expect(this.alphasea.connect(this.otherAddress).refundPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+      }]))
+          .to.be.revertedWith('refundPurchase is forbidden now');
     });
-    it("purchase not exist", async function () {
-    });
+
     it("already shipped", async function () {
+      await ethers.provider.send("evm_setNextBlockTimestamp", [this.shippingStartAt])
+      await (await this.alphasea.shipPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+        purchaser: this.otherAddress.address,
+        encryptedContentKey: [1, 2, 3],
+      }])).wait()
+
+      await ethers.provider.send("evm_setNextBlockTimestamp", [this.refundStartAt])
+      await expect(this.alphasea.connect(this.otherAddress).refundPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+      }]))
+          .to.be.revertedWith('Already shipped.');
     });
+
     it("already refunded", async function () {
+      await (await this.alphasea.connect(this.otherAddress).refundPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+      }])).wait()
+
+      await expect(this.alphasea.connect(this.otherAddress).refundPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+      }]))
+          .to.be.revertedWith('Already refunded.');
     });
+
     it("reentrance", async function () {
+      // TODO
     });
+
     it("ok", async function () {
+      await expect(this.alphasea.connect(this.otherAddress).refundPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+      }, {
+        modelId: 'model2',
+        executionStartAt: this.executionStartAt,
+      }]))
+          .to.emit(this.alphasea, 'PurchaseRefunded')
+          .withArgs(
+              'model1',
+              this.executionStartAt,
+              this.otherAddress.address,
+          )
+          .to.emit(this.alphasea, 'PurchaseRefunded')
+          .withArgs(
+              'model2',
+              this.executionStartAt,
+              this.otherAddress.address,
+          )
+    });
+
+    it("ok. check balance", async function () {
+      await expect(await this.alphasea.connect(this.otherAddress).refundPurchases([{
+        modelId: 'model1',
+        executionStartAt: this.executionStartAt,
+      }, {
+        modelId: 'model2',
+        executionStartAt: this.executionStartAt,
+      }]))
+          .to.changeEtherBalances(
+              [this.otherAddress, this.alphasea],
+              [3, -3]
+          )
     });
   })
 });
