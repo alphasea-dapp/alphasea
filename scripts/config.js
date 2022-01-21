9f36919686999627ae63f38550ceac4472ac4d4d
a <a@example.com>
2022-01-22 01:11:56 +0900

    increase tournament count 12

--- scripts/config.js
+++ scripts/config.js
@@ -1,16 +1,19 @@
-const tournaments = [
-    {
-        'tournamentId': 'crypto_daily',
-        'executionStartAt': 30 * 60, // 0:30UTC
+
+const _ = require('lodash');
+
+const tournaments = _.map(_.range(0, 24, 2), (i) => {
+    return {
+        'tournamentId': 'crypto_daily_' + ('00' + i).slice(-2) + '30',
+        'executionStartAt': 30 * 60 + i * 60 * 60, // i:30UTC
         'predictionTime': 8 * 60,
         'purchaseTime': 8 * 60,
         'shippingTime': 8 * 60,
         'executionPreparationTime': 6 * 60,
-        'executionTime': 60 * 60,
+        'executionTime': 2 * 60 * 60,
         'publicationTime': 15 * 60,
         'description': 'https://github.com/alphasea-dapp/alphasea/tree/master/tournaments/crypto_daily.md',
     }
-];
+})
 
 module.exports = {
     tournaments: tournaments,
